fb3fa65caaf4529c4deff3a693f905033c625899
ralvarezklabs <ralvarez@kreilabs.com>
2022-05-24 11:03:24 -0300

    test to build in pipeline azure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TablePivotComponent } from './components/table-pivot/table-pivot.component';
 
 const routes: Routes = [
-  // {path:'', component: AppComponent },
-  {
-    path: 'pivot',
-    component: TablePivotComponent,
-    loadChildren: () =>
-      import('./components/table-pivot/table-pivot.module').then((m) => m.TablePivotModule),
-  }
+  {path:'', component: AppComponent },
+  // {
+  //   path: 'pivot',
+  //   component: TablePivotComponent,
+  //   loadChildren: () =>
+  //     import('./components/table-pivot/table-pivot.module').then((m) => m.TablePivotModule),
+  // }
 ];
 
 @NgModule({
